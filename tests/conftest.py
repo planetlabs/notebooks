9b84da51808a5ec5a18406c4d819747a9dd773e6
jreiberkyle <jreiberkyle@users.noreply.github.com>
2018-10-23 16:12:51 -0700

    add test parameter, path, which allows running tests on notebooks only within that directory

---
+++ tests/conftest.py
@@ -0,0 +1,64 @@
+import os
+from pathlib import Path
+
+import pytest
+
+NOTEBOOK_EXT = '.ipynb'
+ROOT_DIR = 'jupyter-notebooks'
+
+# do not look for notebooks in .ipynb_checkpoints dir
+SKIP_DIRECTORIES = ['.ipynb_checkpoints']
+
+def pytest_addoption(parser):
+    parser.addoption("--path", action="store",
+                     default=ROOT_DIR,
+                     help="root directory of notebooks to test")
+
+
+def pytest_generate_tests(metafunc):
+    '''Generate a test for every notebook found within the root directory.'''
+    option_path = metafunc.config.option.path
+    if 'notebook_path' in metafunc.fixturenames and option_path is not None:
+        root_path = normalize_path(option_path)
+        notebook_paths = find_notebooks(root_path)
+        metafunc.parametrize('notebook_path', notebook_paths)
+
+
+def normalize_path(path):
+    '''If specified path is relative to the ROOT_DIR, then make absolute.'''
+    path_parts = list(Path(path).parts)
+    if path_parts[0] != ROOT_DIR:
+        path_parts = [ROOT_DIR] + path_parts
+
+    return os.path.join(*path_parts)
+
+
+def find_notebooks(root_dir):
+    '''Find all of the notebooks within the root directory.
+
+    Skip notebooks and subdirectories of any directory with 'norun' file
+    '''
+    print('finding notebooks in {}...'.format(root_dir))
+    notebooks = []
+
+    for (dirpath, dirnames, filenames) in os.walk(root_dir):
+        # look for 'norun' file, if present, do not look for notebooks in this dir
+        if 'norun' in filenames:
+            # do not look into subdirectories, modify in-place for walk()
+            dirnames[:] = []
+
+            # clear filenames
+            filenames = []
+
+        # modify in place to filter subdirectories in walk()
+        # https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
+        dirnames[:] = [n for n in dirnames if n not in SKIP_DIRECTORIES]
+
+        for name in filenames:
+            ext = os.path.splitext(name)[1]
+            if ext == NOTEBOOK_EXT:
+                # convert to string so test displays the path
+                notebook_name = str(os.path.join(dirpath, name))
+                notebooks.append(notebook_name)
+    print('found {} notebooks'.format(len(notebooks)))
+    return notebooks
--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -7,45 +7,19 @@ pytest runs each test as a unit test.
 """
 import os
 import subprocess
+import logging
 import tempfile
 
 import pytest
 
 NOTEBOOK_EXT = '.ipynb'
-ROOT_DIR = 'jupyter-notebooks'
 
 
-def find_notebooks(root_dir):
-    print('finding notebooks...')
-    notebooks = []
-    for (dirpath, dirnames, filenames) in os.walk(root_dir):
-        # look for 'norun' file, if present, do not look for notebooks in this dir
-        if 'norun' in filenames:
-            # do not look into subdirectories, modify in-place for walk()
-            dirnames[:] = []
-
-            # clear filenames
-            filenames = []
-
-        # modify in place to filter subdirectories in walk()
-        # https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
-        dirnames[:] = [n for n in dirnames if n != '.ipynb_checkpoints']
-
-        for name in filenames:
-            ext = os.path.splitext(name)[1]
-            if ext == NOTEBOOK_EXT:
-                # convert to string so test displays the path
-                notebook_name = str(os.path.join(dirpath, name))
-                notebooks.append(notebook_name)
-    print('found {} notebooks'.format(len(notebooks)))
-    return notebooks
-
-
-@pytest.mark.parametrize("notebook", find_notebooks(ROOT_DIR))
-def test_run_notebooks(notebook):
+def test_run_notebooks(notebook_path):
+    '''Test that the notebook runs successfully'''
     with tempfile.NamedTemporaryFile(suffix=NOTEBOOK_EXT) as tmp_nb:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=600",
-                notebook, "--output", tmp_nb.name]
+                notebook_path, "--output", tmp_nb.name]
         print(' '.join(args))
         subprocess.check_call(args)
